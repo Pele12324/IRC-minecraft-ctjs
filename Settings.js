c26174509850d21edc569999df201ccafc6aedda
Pele12324 <48065727+Pele12324@users.noreply.github.com>
2024-10-14 20:08:36 +0200

    New IP

--- Settings.js
+++ Settings.js
@@ -14,7 +14,7 @@ class Settings {
         description: "Ip for IRC server. (GitHub for self-hosting the server coming soon.)",
         category: "IRC"
     })
-    wsIP= "wss://8f81fe8d-7e50-44ac-923c-5c407de08e4d-00-1ow5gxno6ewju.worf.replit.dev"
+    wsIP= "wss://irc-minecraft-server.onrender.com"
 
     constructor() {
         this.initialize(this)
